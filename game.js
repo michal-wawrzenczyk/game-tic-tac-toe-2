7a12c03ee28e36afc65aec90e221d5f4994fb61c
Michał Wawrzeńczyk <wawrzenczyk.m@yahoo.com>
2021-12-12 12:53:46 +0100

    feat(game-board): find the clicked field, add player symbol, add disabled style for list items, switch player

--- game.js
+++ game.js
@@ -6,6 +6,30 @@ function startNewGame() {
         alert('Please set custom player names!');
         return; // prevent execution of further code.
     }
+    // show active player name which has the first move
+    activePlayerNameElement.textContent = players[activePlayer].name;
     // make the game field visible
     gameAreaElement.style.display = 'block'; // this will display the game board when the button was clicked. But it should do that only when the names of players are valid.
+}
+
+function switchPlayer() {
+    if (activePlayer === 0) {
+        activePlayer = 1;
+    } else { // if active player is not one, then switch to 0 - First player with symbol X
+        activePlayer = 0;
+    }
+    // change the Player Name (update text shown in the span element) whenever we switched a player
+    activePlayerNameElement.textContent = players[activePlayer].name // name property to store the name of player
+}
+
+function selectGameField(event) {
+    // find out which field was clicked
+    // add symbol of the active player
+    event.target.textContent = players[activePlayer].symbol; // to dynamically access the active player index. For first turn it will be players[0] => First player
+    // add disable class once a symbol was placed in the field
+    event.target.classList.add('disabled'); // li.disabled in game.css file
+    // switch player turn
+    switchPlayer(); // execute it when game field was selected - it works, but we can change the symbol in one field more than once, which is wrong.
+    // don't select already occupied field
+    // keep track of all the selected fields
 }
